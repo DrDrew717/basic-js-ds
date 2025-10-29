7c2830c7d0292b720d7e3850e784048f3e259472
DrDrew717 <andyak0717@gmail.com>
2025-10-30 02:01:24 +0600

    feat: add solution to stack task

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,20 @@ const { NotImplementedError } = require('../lib/errors');
  *
  */
 class Stack {
-  push(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor() {
+    this.array = [];
+  }
+
+  push(value) {
+    this.array.push(value);
   }
 
   pop() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.array.pop();
   }
 
   peek() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.array[this.array.length - 1];
   }
 }
 
